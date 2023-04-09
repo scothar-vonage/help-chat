16a07857ce042ff5ee3b471accb268099b8ee669
Scott Harris <sharris@twilio.com>
2023-04-09 12:30:30 -0700

    ingest markdown files

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -5,26 +5,30 @@ import { pinecone } from '@/utils/pinecone-client';
 import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders';
+import { TextLoader } from "langchain/document_loaders";
+import { MarkdownTextSplitter } from "langchain/text_splitter";
 
 /* Name of directory to retrieve your files from */
-const filePath = 'docs';
+const filePath = 'docs/vonage';
 
 export const run = async () => {
   try {
     /*load raw docs from the all files in the directory */
+    // const directoryLoader = new DirectoryLoader(filePath, {
+    //   '.pdf': (path) => new CustomPDFLoader(path),
+    // });
+
     const directoryLoader = new DirectoryLoader(filePath, {
-      '.pdf': (path) => new CustomPDFLoader(path),
+      '.txt': (path) => new TextLoader(path),
     });
 
     // const loader = new PDFLoader(filePath);
     const rawDocs = await directoryLoader.load();
 
     /* Split text into chunks */
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
-    });
+    const textSplitter =  new MarkdownTextSplitter();
 
+    //const docs = await textSplitter.splitDocuments(rawDocs);
     const docs = await textSplitter.splitDocuments(rawDocs);
     console.log('split docs', docs);
 
@@ -33,12 +37,21 @@ export const run = async () => {
     const embeddings = new OpenAIEmbeddings();
     const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
 
+    console.log('Embeding pdf docs...');
     //embed the PDF documents
+
+    //const batchSize = 10;
+    //const numBatches = Math.ceil(docs.length / batchSize);
+
+    //for (let i = 0; i < numBatches; i++) {
+    //  console.log(`Processing batch ${i} of ${numBatches}`);
+    //  const batchDocs = docs.slice(i * batchSize, (i + 1) * batchSize);
     await PineconeStore.fromDocuments(docs, embeddings, {
       pineconeIndex: index,
       namespace: PINECONE_NAME_SPACE,
       textKey: 'text',
     });
+    // }
   } catch (error) {
     console.log('error', error);
     throw new Error('Failed to ingest your data');
