bb44696a854d33baf9bf6ebf5ec39a6bad026a9e
Scott Harris <sharris@twilio.com>
2023-05-17 14:29:48 -0700

    updated file location

--- scripts/ingest-data.ts
+++ scripts/ingest-data.ts
@@ -2,14 +2,12 @@ import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { OpenAIEmbeddings } from 'langchain/embeddings';
 import { PineconeStore } from 'langchain/vectorstores';
 import { pinecone } from '@/utils/pinecone-client';
-import { CustomPDFLoader } from '@/utils/customPDFLoader';
 import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from '@/config/pinecone';
 import { DirectoryLoader } from 'langchain/document_loaders';
 import { TextLoader } from "langchain/document_loaders";
-import { MarkdownTextSplitter } from "langchain/text_splitter";
 
 /* Name of directory to retrieve your files from */
-const filePath = 'docs/vonage/api';
+const filePath = 'docs/text';
 
 export const run = async () => {
   try {
@@ -45,7 +43,7 @@ export const run = async () => {
     // }
   } catch (error) {
     console.log('error', error);
-    throw new Error('Failed to ingest your data');
+    throw new Error('Failed to ingest data');
   }
 };
 
